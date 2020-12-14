d3952833c1a1b67221ac023de7baec242b4c4005
alanchn31 <alanchn31@gmail.com>
2020-12-14 23:07:16 +0800

    Added unit testing for function that handles date

--- tests/test_model_train.py
+++ tests/test_model_train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pyspark.sql.types as typ
 import pytest
 from app.jobs import model_train
+from datetime import date
 
 class TestModelTrainJob:
     def test_remove_duplicates(self, spark_session):
@@ -93,7 +94,35 @@ class TestModelTrainJob:
              ['ID', 'Age']
         ).toPandas()
 
-        real_data = model_train._remove_outliers(test_data, test_config).toPandas()
+        real_data = model_train._impute_outliers(test_data, test_config).toPandas()
+
+        pd.testing.assert_frame_equal(real_data,
+                                      expected_data,
+                                      check_dtype=False)
+    
+
+    def test_convert_str_to_date(self, spark_session):
+        test_data = spark_session.createDataFrame(
+            [("1/1/84", "3/8/18"),
+             ("9/12/77", "26/9/18"),
+             ("1/6/00", "16/9/18")],
+             ['DOB', 'DISBURSED_DATE']
+        )
+
+        test_config = {
+            "date_str_cols": {"DOB": "19",
+                              "DISBURSED_DATE": "20"
+                             }
+        }
+
+        expected_data = pd.DataFrame(
+            [(date(1984, 1, 1), date(2018, 8, 3)),
+             (date(1977, 12, 9), date(2018, 9, 26)),
+             (date(2000, 6, 1), date(2018, 9, 16))],
+             columns=['DOB', 'DISBURSED_DATE']
+        )
+
+        real_data = model_train._convert_str_to_date(test_data, test_config).toPandas()
 
         pd.testing.assert_frame_equal(real_data,
                                       expected_data,
